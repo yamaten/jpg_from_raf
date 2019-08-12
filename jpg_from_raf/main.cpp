e6eeb9d29b33d62c88ff60cc77116076255a5758
てんすやまもも <sloth.ten@gmail.com>
2019-08-12 19:44:25 +0900

    Initial Commit

---
+++ jpg_from_raf/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  jpg_from_raf
+//
+//  Created by YAMAMOTO TENSHI on 2019/08/12.
+//  Copyright © 2019 YAMAMOTO TENSHI. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
